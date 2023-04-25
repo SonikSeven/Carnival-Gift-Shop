3a5bd651653ab90601d2193fc1945e78110086c2
Mikhail Semenov <106490807+SonikSeven@users.noreply.github.com>
2023-04-25 16:19:52 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+function displayGifts(gifts) {
+    console.log("Here's the list of gifts:\n");
+    for (const [id, gift] of Object.entries(gifts)) {
+        console.log(`${id}- ${gift["name"]}, Cost: ${gift["cost"]} tickets`);
+    }
+}
+
+function showTickets(tickets) {
+    console.log(`Total tickets: ${tickets["amount"]}`);
+}
+
+function buyGift(input, gifts, tickets) {
+    if (Object.keys(gifts).length === 0) {
+        return console.log("Wow! There are no gifts to buy.");
+    }
+    let answer = Number(input("Enter the number of the gift you want to get:"));
+    let gift = gifts[answer];
+    if (isNaN(answer)) {
+        return console.log("Please enter a valid number!");
+    } else if (typeof gift === "undefined") {
+        return console.log("There is no gift with that number!");
+    } else if (gift["cost"] > tickets["amount"]) {
+        return console.log("You don't have enough tickets to buy this gift.");
+    }
+    tickets["amount"] = tickets["amount"] - gift["cost"];
+    console.log(`Here you go, one ${gift["name"]}!`);
+    delete gifts[answer];
+    showTickets(tickets);
+}
+
+function addTickets(input, tickets) {
+    let answer = Number(input("Enter the ticket amount: "));
+    if (isNaN(answer) || (answer < 0) || (answer > 1000)) {
+        return console.log("Please enter a valid number between 0 and 1000.");
+    }
+    tickets["amount"] = tickets["amount"] + answer;
+    showTickets(tickets);
+}
+
+function main() {
+    const input = require('sync-input');
+    let tickets = {"amount": 0};
+    let gifts = {
+        1: {name: "Teddy Bear", cost: 10},
+        2: {name: "Big Red Ball", cost: 5},
+        3: {name: "Huge Bear", cost: 50},
+        4: {name: "Candy", cost: 8},
+        5: {name: "Stuffed Tiger", cost: 15},
+        6: {name: "Stuffed Dragon", cost: 30},
+        7: {name: "Skateboard", cost: 100},
+        8: {name: "Toy Car", cost: 25},
+        9: {name: "Basketball", cost: 20},
+        10: {name: "Scary Mask", cost: 75}
+    };
+
+    console.log(`WELCOME TO THE CARNIVAL GIFT SHOP!
+Hello friend! Thank you for visiting the carnival!`);
+    displayGifts(gifts);
+
+    while (true) {
+        switch (Number(input(`\nWhat do you want to do?
+1-Buy a gift 2-Add tickets 3-Check tickets 4-Show gifts 5-Exit the shop\n`))) {
+            case 1:
+                buyGift(input, gifts, tickets);
+                continue;
+            case 2:
+                addTickets(input, tickets);
+                continue;
+            case 3:
+                showTickets(tickets);
+                continue;
+            case 4:
+                displayGifts(gifts);
+                continue;
+            case 5:
+                return console.log("Have a nice day!");
+            default:
+                console.log("Please enter a valid number!");
+        }
+    }
+}
+
+main();
